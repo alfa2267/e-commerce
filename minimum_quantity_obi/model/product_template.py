156c401112e09c19fd999d2e933551dbe1e3c798
alfa2267 <oceaniaalfa@gmail.com>
2020-02-17 02:59:22 +0000

    Add files via upload

---
+++ minimum_quantity_obi/model/product_template.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2019 Obi Bakes It <https://obibakes.it>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+
+from odoo import api,fields,models,_
+
+class ProductTemplate(models.Model):
+    _inherit="product.template"
+
+    minimum_order_quantity = fields.Float(string='Minimum Order Quantity',help="This field display minimum order qunatity")
+    
+    set_min_order_qty_eshop = fields.Boolean(
+        'Set Minimum Order Amount', default = False, 
+        help="If checked, choosen minimum order amount will be set. Customers will only be able to order minimum amount or greater.")
